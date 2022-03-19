28fae51463d8a2f7c71f2538f684d11565fd3ed5
‍이세희(학부생-산림환경시스템학과) <tpfktpgml24@kookmin.ac.kr>
2022-03-19 06:20:07 +0000

    수정

--- If_Switch/if.cpp
+++ If_Switch/if.cpp
@@ -4,14 +4,16 @@ using namespace std;
 int main(){
     int number; 
     cout << "점수 입력하십쇼.: ";
-    cin << number;
+    cin >> number;
 
     if (number >= 90) 
         cout << "잘하는군!\n";
-    else if (number >= 70) {
+	
+    else if (number >= 70) 
         cout << "쫌 하네\n";
+	
     else 
         cout << "흠.. 그래\n";
-    }
+    
     return 0;
 }
