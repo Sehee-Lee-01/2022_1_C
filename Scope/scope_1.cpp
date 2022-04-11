5b2f51d055c0639a0553c4d1565b6a079fd8e2de
‍이세희(학부생-산림환경시스템학과) <tpfktpgml24@kookmin.ac.kr>
2022-04-11 01:40:19 +0000

    22.04.11

--- Scope/scope_1.cpp
+++ Scope/scope_1.cpp
@@ -6,20 +6,25 @@ using namespace std;
 
 // scope_2.cpp 프로토타입 선언. 컴파일 순서 때문에 선언 필요.
 // void f2(void); 
+//int f1(int a, int b=1); 
 
-int f1(int a, int b=1); 
+/*g1은 프로그램 시작부터 끝날 때까지 lifetime 지속*/
 int g1 = 3;
 
 // 참고) 아래와 같이 전역변수를 선언하게 된다면 다른 파일에서 참조 못함.
 // static int f1(int a, int b=1); 
 // static int g1 = 3;
 
+/*garray는 프로그램 시작부터 끝날 때까지 lifetime 지속*/
 extern int garray[3]; // scope_2.cpp
 
+/*main은 전역 scope*/
 int main(){
+	/*i의 scope는 main 블록 안*/
 	extern int i; // scope_2.cpp
 	
 	for(int j=0; j<2; j++){
+		/*아래의 i의 scope는 for 블록 안. 바로 이전 선언 i는 가려짐.*/
 		static int i =1;
 		i++;
 		cout << "inside loop, i = " << i << endl;
@@ -28,11 +33,15 @@ int main(){
 	
 	cout << "f1(i) = " << f1(i) << endl;
 	cout << "f1(i) = " << f1(1000,50) << endl;
-    f2();
+	
+    /*f2의 scope는 main 블록 안*/
+	f2();
 	return 0;
 } 
 
+/*f1은 전역 scope*/
 int f1(int a, int b){
+	/*a, b, c scope는 f1 블록 안*/
 	static int c = 100;
 	c = a+b+c+g1;
 	return c;
--- Scope/scope_2.cpp
+++ Scope/scope_2.cpp
@@ -7,10 +7,13 @@ using namespace std;
 // scope_1.cpp 프로토타입 선언. 컴파일 순서 때문에 선언 필요.
 // int f1(int a, int b=1); 
 
+/*garray와 i는 프로그램 시작부터 끝날 때까지 lifetime 지속*/
 int garray[3];
 int i;
 
+/*f2는 전역 scope*/
 void f2(void){
+	/*g1의 scope는 f2 블록 안*/
 	extern int g1; // scope_1.cpp
 	cout << "f1(g1,20) = " << f1(g1,20) << endl;
 }
