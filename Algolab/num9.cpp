e7d0e74537fd981dcda9d68fd0f8e0eaf5206bb3
‍이세희(학부생-산림환경시스템학과) <tpfktpgml24@kookmin.ac.kr>
2022-04-12 09:21:43 +0000

    주가분석 완료

--- Algolab/num9.cpp
+++ Algolab/num9.cpp
@@ -5,28 +5,23 @@ Problem: 주가분석-1
 #include <iostream>
 using namespace std;
 
-// 배열사용 금지 
-
 int main(void){
-    // 입력 받기
     int numTestCases;
     cin >> numTestCases;
 
     for(int i=0; i<numTestCases; i++)
     {
-        int numData, data;
-        cin >> numData;
-        // 이전 값 선언
-        // 이후 값 선언
-        // 중간 고점 값 선언(이전 < 현재, 이후 < 현재, 연속되는 것은 연속의 마지막에서)
-        // 중간 고점 횟수(중간 고점 값 바뀔 때 더하기)
-        for (int j=0; j<numData; j++)
+        unsigned int numData, prev, curr;
+        // 임의로 정해지는 middlemax 주의
+        unsigned int result = 0, middlemax = 0;
+        cin >> numData >> prev;
+        for (int j=0; j<numData-1; j++)
         {
-            cin >> data;
-            // 중간 고점 정하는 알고리즘 
+            cin >> curr;
+            if (prev < curr){middlemax = curr;}
+            if ((middlemax == prev) && (prev > curr)){result++;}
+            prev = curr;
         }
-        // 출력 하기
-        cout << "result" << endl;
+        cout << result << endl;
     }
-    return 0;    
 }
