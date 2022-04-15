963bdc7baf46d095fbebdf19c7b95a6dc2b9c3e4
‍이세희(학부생-산림환경시스템학과) <tpfktpgml24@kookmin.ac.kr>
2022-04-15 10:12:30 +0000

    22.04.15

--- Expression/callByValue3.cpp
+++ Expression/callByValue3.cpp
@@ -5,7 +5,6 @@ using namespace std;
 // call by value
 // void f2(int a, int b, int sum, int diff);
 
-//call by reference
 int f2(int a, int b=0, int *sum=nullptr, int *diff=nullptr);
 
 int main(){
@@ -13,7 +12,6 @@ int main(){
     // call by value
     //f2(i, j, k , l); 
     
-    //call by reference
     f2(i, j, &k , &l);
     cout << "k = " << k << endl;
     cout << "l = " << l << endl;
@@ -27,7 +25,6 @@ void f2(int a, int b, int sum, int diff){
 }
 */
 
-//call by reference
 int f2(int a, int b, int *sum, int *diff){
     // nullptr 일시 오류 방지
     if (sum==nullptr){
